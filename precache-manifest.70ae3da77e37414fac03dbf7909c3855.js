e4d7b9f1034eb0d5d0fa0c387ec8e43b455c8508
lionbitrade <guillem.lion.benosa@gmail.com>
2019-01-22 00:00:06 +0700

    Updates

---
+++ precache-manifest.70ae3da77e37414fac03dbf7909c3855.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a9ae4b7ba126e7710143348819228b3",
+    "url": "/xchange-rates/static/media/loader-dots.0a9ae4b7.gif"
+  },
+  {
+    "revision": "f76d6e9169147634dc4a71713f28b3e0",
+    "url": "/xchange-rates/static/media/link.f76d6e91.svg"
+  },
+  {
+    "revision": "a1bdcc9a9b8f65ac92a491fc37128cf3",
+    "url": "/xchange-rates/static/media/giveth-white-min.a1bdcc9a.png"
+  },
+  {
+    "revision": "8b2307f4def94de5bbb1832eb8f31117",
+    "url": "/xchange-rates/static/media/exchange2.8b2307f4.svg"
+  },
+  {
+    "revision": "60222bdcd22613bf56e8",
+    "url": "/xchange-rates/static/js/runtime~main.60222bdc.js"
+  },
+  {
+    "revision": "6b804d55eba3c265c1c4",
+    "url": "/xchange-rates/static/js/main.6b804d55.chunk.js"
+  },
+  {
+    "revision": "9a653b641b206186cf0f",
+    "url": "/xchange-rates/static/js/1.9a653b64.chunk.js"
+  },
+  {
+    "revision": "6b804d55eba3c265c1c4",
+    "url": "/xchange-rates/static/css/main.4af5e4e4.chunk.css"
+  },
+  {
+    "revision": "9a653b641b206186cf0f",
+    "url": "/xchange-rates/static/css/1.9d7d8929.chunk.css"
+  },
+  {
+    "revision": "b80ea1cb7988678920e24068440e4340",
+    "url": "/xchange-rates/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,34 @@
-"use strict";var precacheConfig=[["/xchange-rates/index.html","cc23c5a74f81bce8de16adaabeea6bdf"],["/xchange-rates/static/css/main.1b0f8194.css","57003494bff243c487c7cd07ed16d9ea"],["/xchange-rates/static/js/main.524efbe4.js","bbaeca4a8795eb69387204e828ccfc2e"],["/xchange-rates/static/media/exchange2.8b2307f4.svg","8b2307f4def94de5bbb1832eb8f31117"],["/xchange-rates/static/media/giveth-white-min.a1bdcc9a.png","a1bdcc9a9b8f65ac92a491fc37128cf3"],["/xchange-rates/static/media/link.f76d6e91.svg","f76d6e9169147634dc4a71713f28b3e0"],["/xchange-rates/static/media/loader-dots.0a9ae4b7.gif","0a9ae4b7ba126e7710143348819228b3"]],cacheName="sw-precache-v3-sw-precache-webpack-plugin-"+(self.registration?self.registration.scope:""),ignoreUrlParametersMatching=[/^utm_/],addDirectoryIndex=function(e,t){var n=new URL(e);return"/"===n.pathname.slice(-1)&&(n.pathname+=t),n.toString()},cleanResponse=function(t){return t.redirected?("body"in t?Promise.resolve(t.body):t.blob()).then(function(e){return new Response(e,{headers:t.headers,status:t.status,statusText:t.statusText})}):Promise.resolve(t)},createCacheKey=function(e,t,n,r){var a=new URL(e);return r&&a.pathname.match(r)||(a.search+=(a.search?"&":"")+encodeURIComponent(t)+"="+encodeURIComponent(n)),a.toString()},isPathWhitelisted=function(e,t){if(0===e.length)return!0;var n=new URL(t).pathname;return e.some(function(e){return n.match(e)})},stripIgnoredUrlParameters=function(e,n){var t=new URL(e);return t.hash="",t.search=t.search.slice(1).split("&").map(function(e){return e.split("=")}).filter(function(t){return n.every(function(e){return!e.test(t[0])})}).map(function(e){return e.join("=")}).join("&"),t.toString()},hashParamName="_sw-precache",urlsToCacheKeys=new Map(precacheConfig.map(function(e){var t=e[0],n=e[1],r=new URL(t,self.location),a=createCacheKey(r,hashParamName,n,/\.\w{8}\./);return[r.toString(),a]}));function setOfCachedUrls(e){return e.keys().then(function(e){return e.map(function(e){return e.url})}).then(function(e){return new Set(e)})}self.addEventListener("install",function(e){e.waitUntil(caches.open(cacheName).then(function(r){return setOfCachedUrls(r).then(function(n){return Promise.all(Array.from(urlsToCacheKeys.values()).map(function(t){if(!n.has(t)){var e=new Request(t,{credentials:"same-origin"});return fetch(e).then(function(e){if(!e.ok)throw new Error("Request for "+t+" returned a response with status "+e.status);return cleanResponse(e).then(function(e){return r.put(t,e)})})}}))})}).then(function(){return self.skipWaiting()}))}),self.addEventListener("activate",function(e){var n=new Set(urlsToCacheKeys.values());e.waitUntil(caches.open(cacheName).then(function(t){return t.keys().then(function(e){return Promise.all(e.map(function(e){if(!n.has(e.url))return t.delete(e)}))})}).then(function(){return self.clients.claim()}))}),self.addEventListener("fetch",function(t){if("GET"===t.request.method){var e,n=stripIgnoredUrlParameters(t.request.url,ignoreUrlParametersMatching),r="index.html";(e=urlsToCacheKeys.has(n))||(n=addDirectoryIndex(n,r),e=urlsToCacheKeys.has(n));var a="/xchange-rates/index.html";!e&&"navigate"===t.request.mode&&isPathWhitelisted(["^(?!\\/__).*"],t.request.url)&&(n=new URL(a,self.location).toString(),e=urlsToCacheKeys.has(n)),e&&t.respondWith(caches.open(cacheName).then(function(e){return e.match(urlsToCacheKeys.get(n)).then(function(e){if(e)return e;throw Error("The cached response that was expected is missing.")})}).catch(function(e){return console.warn('Couldn\'t serve response for "%s" from cache: %O',t.request.url,e),fetch(t.request)}))}});
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/xchange-rates/precache-manifest.70ae3da77e37414fac03dbf7909c3855.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/xchange-rates/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
